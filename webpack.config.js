d1753c8def8aa4dff9bcf92679b51acb52bd2419
Maria Pinto Nogueira <64786262+nogueiram-27@users.noreply.github.com>
2021-03-07 10:29:29 +0000

    added webpack and babel configurations

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+            {
+                loader: 'babel-loader',
+            }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+      extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html'
+    })
+  ]
+};
